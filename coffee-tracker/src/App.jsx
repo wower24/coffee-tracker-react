db603708c1731fa0b3cc5c0c1237b1b0b857a325
Marta Wower <marta.wower24@gmail.com>
2025-10-02 12:43:41 -0600

    general logic

--- coffee-tracker/src/App.jsx
+++ coffee-tracker/src/App.jsx
@@ -2,16 +2,28 @@ import { useState } from 'react'
 import Layout from './components/Layout'
 import Hero from './components/Hero'
 import CoffeeForm from './components/CoffeeForm'
+import Stats from './components/Stats'
+import History from './components/History'
 
 
 function App() {
 
   const isAuthenticated = false
 
+  const authenticatedContent = (
+    <>
+      <Stats />
+      <History />
+    </>
+  )
+
   return (
     <Layout>
       <Hero />
       <CoffeeForm />
+      {isAuthenticated && (
+        authenticatedContent
+      )}
     </Layout>
   )
 }
