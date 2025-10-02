1a654ee2eae751b0f8cacc0f4c2486397e967675
Marta Wower <marta.wower24@gmail.com>
2025-10-02 14:38:27 -0600

    Header and Footer

--- coffee-tracker/src/components/Layout.jsx
+++ coffee-tracker/src/components/Layout.jsx
@@ -2,11 +2,30 @@ export default function Layout(props) {
     const { children } = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1>CAFFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i class="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <p>
+                <span className='text-gradient'>
+                    Caffiend
+                </span> was made by <a 
+                href='https://www.wower.dev'
+                target='_blank'>wower.code</a> using 
+                the <a 
+                href='https://www.fantacss.smoljames.com'
+                target='_blank'>FantaCSS</a> design library.</p>
+        </footer>
     )
     return (
         <>
