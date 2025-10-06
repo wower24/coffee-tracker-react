60bb62b12fb83eeef82d02d72186bd0c30a500be
Marta Wower <marta.wower24@gmail.com>
2025-10-06 17:53:45 -0600

    History

--- coffee-tracker/src/components/History.jsx
+++ coffee-tracker/src/components/History.jsx
@@ -1,7 +1,37 @@
+import { calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getCaffeineAmount, timeSinceConsumption } from "../utils";
+
 export default function History() {
     return (
         <>
-        
+            <div className='section-header'>
+                <i className='fa-solid fa-timeline'/>
+                <h2>History</h2>
+            </div>
+            <p><i>Hover for more information</i></p>
+            <div className='coffee-history'>
+                {
+                    Object.keys(coffeeConsumptionHistory).sort((a, b) => b - a)
+                    .map((utcTime, coffeeIndex) => {
+                        const coffee = coffeeConsumptionHistory[utcTime]
+                        const timeSinceConsume = timeSinceConsumption(utcTime)
+                        const originalAmount = getCaffeineAmount(coffee.name)
+                        const remainingAmount = calculateCurrentCaffeineLevel({
+                            [utcTime] : coffee
+                        })
+
+                        const summary = `${coffee.name} | ${timeSinceConsume} | 
+                        ${originalAmount}mg / ${remainingAmount}mg | $${coffee.cost}`
+
+                        return(
+                            <div 
+                            title={summary}
+                            key={coffeeIndex}>
+                                <i className='fa-solid fa-mug-hot'/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
